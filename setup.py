ca131d2fc09e10ae461a4359e2b3759eed60cfec
Daniel Bienert <b13n3rd@bienerd.de>
2018-08-02 08:06:51 +0200

    Update setup.py
    Addes pyspark, tensorflow dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='elephas',
-      version='0.3',
+      version='0.3-bp0.3.1',
       description='Deep learning on Spark with Keras',
-      url='http://github.com/maxpumperla/elephas',
+      url='https://github.com/b13n3rd/elephas',
       download_url='https://github.com/maxpumperla/elephas/tarball/0.3',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['keras', 'hyperas', 'flask'],
+      install_requires=['keras', 'hyperas', 'flask', 'pyspark' ,'tensorflow'],
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
